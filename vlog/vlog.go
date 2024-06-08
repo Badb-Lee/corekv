84902f98be0928f362ff21cdb6dcc125dde3a275
Lee <350006796@qq.com>
2024-06-08 13:52:28 +0800

    init: vlog

---
+++ vlog/vlog.go
@@ -0,0 +1,39 @@
+package vlog
+
+import (
+	"corekv/utils"
+	"corekv/utils/codec"
+)
+
+type Options struct {
+}
+
+type VLog struct {
+	closer *utils.Closer
+}
+
+// NewVLog 新建VLog
+func NewVLog() *VLog {
+	v := &VLog{}
+	v.closer = utils.NewCloser(1)
+	return v
+}
+
+func (v *VLog) StartGC() {
+	defer v.closer.Done()
+	for {
+		select {
+		case <-v.closer.Wait():
+
+		}
+	}
+}
+
+// Set 进行kv分离
+func (v *VLog) Set(entry *codec.Entry) error {
+	return nil
+}
+
+func (v *VLog) Get(entry *codec.Entry) (*codec.Entry, error) {
+	return nil, nil
+}
