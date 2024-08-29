ab01f605130db1c971047427527b26957ec8cd4d
Lee <350006796@qq.com>
2024-08-29 17:09:35 +0800

    feat: final_lab_tips

--- db_test.go
+++ db_test.go
@@ -80,3 +80,44 @@ func TestAPI(t *testing.T) {
 	}
 
 }
+
+func FuzzAPI(f *testing.F) {
+	f.Add([]byte("core"), []byte("kv"))
+	clearDir()
+	db := Open(opt)
+	opt.ValueLogFileSize = 1 << 20
+	opt.ValueThreshold = 1 << 20
+	defer func() { _ = db.Close() }()
+	// 运行fuzz引擎，不断生成测试用例进行测试
+	f.Fuzz(func(t *testing.T, key, value []byte) {
+		// 写入
+		e := utils.NewEntry(key, value).WithTTL(100 * time.Second)
+		if err := db.Set(e); err != nil {
+			t.Fatal(err)
+		}
+
+		// 查询
+		if entry, err := db.Get(key); err != nil {
+			t.Fatal(err)
+		} else {
+			t.Logf("db.Get key=%s, value=%s,expiresAt = %d", entry.Key, entry.Value, entry.ExpiresAt)
+		}
+
+		//迭代器
+		iter := db.NewIterator(&utils.Options{
+			IsAsc: false,
+		})
+		defer func() { _ = iter.Close() }()
+		for iter.Rewind(); iter.Valid(); iter.Next() {
+			it := iter.Item()
+			t.Logf("db.NewIterator key=%s, value=%s,expiresAt = %d", it.Entry().Key, it.Entry().Value, it.Entry().ExpiresAt)
+		}
+		t.Logf("db.Stats.EntryNum=%+v", db.Info().EntryNum)
+
+		//删除
+		if err := db.Del([]byte(key)); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+}
